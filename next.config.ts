766f2e920a1e4244a8ed28e8b3965dad18cdbd14
Vibhu D. <125772875+Vrdevil44@users.noreply.github.com>
2025-12-01 21:45:50 -0800

    feat: Add Next.js configuration for static export, unoptimized images, and ignored build errors.

---
+++ next.config.ts
@@ -0,0 +1,22 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+    output: 'export',
+    images: {
+        unoptimized: true,
+    },
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+    },
+};
+
+export default nextConfig;
